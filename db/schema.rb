a7903a26490ae03f7eee569064bbf53623378820
Lawrence Abaeo <labaeo@gmail.com>
2015-06-01 14:59:43 -0700

    Add title to entries

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150601212250) do
+ActiveRecord::Schema.define(version: 20150601215840) do
 
   create_table "categories", force: true do |t|
     t.integer  "contest_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20150601212250) do
     t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
   end
 
   add_index "entries", ["category_id"], name: "index_entries_on_category_id"
