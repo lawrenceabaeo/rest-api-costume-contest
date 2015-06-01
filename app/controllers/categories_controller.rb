f46354ee06ada06f18d4a5cdd50ed258281a164b
Lawrence Abaeo <labaeo@gmail.com>
2015-06-01 16:44:43 -0700

    Whitelist title as a parameter from the user

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
+    @entries = Category.find(params[:id]).entries
   end
 
   # GET /categories/new
--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -69,6 +69,6 @@ class EntriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def entry_params
-      params.require(:entry).permit(:person, :character, :rank, :category_id)
+      params.require(:entry).permit(:person, :character, :rank, :category_id, :title)
     end
 end
