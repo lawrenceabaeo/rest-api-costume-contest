ee3dcc8a4435aa672bf573a5df01b77c199eb975
Lawrence Abaeo <labaeo@gmail.com>
2015-06-02 18:07:20 -0700

    Restrict resource routes for api to just GET actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 RestApiCostumeContest::Application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :contests do
-        resources :categories
+      resources :contests, only: [:index, :show] do
+        resources :categories, only: [:index, :show]
       end
     end
   end
